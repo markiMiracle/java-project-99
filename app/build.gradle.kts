cdb5a318fecdd200c285c43efced05dd8d9042b3
markiMiracle <someonewhosome@list.ru>
2024-08-08 19:44:34 +0300

    add application

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,11 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.3.2"
 	id("io.spring.dependency-management") version "1.1.6"
+	application
+}
+
+application {
+	mainClass.set("hexlet.code.AppApplication")
 }
 
 group = "hexlet.code"
