2c8a3f4e2677acbd8c36bf9115421fa6ab165df7
markiMiracle <someonewhosome@list.ru>
2024-08-13 13:11:28 +0300

    add dataInitializer

---
+++ app/src/main/java/hexlet/code/component/DataInitializer.java
@@ -0,0 +1,26 @@
+package hexlet.code.component;
+
+import hexlet.code.dto.UserCreateDTO;
+import hexlet.code.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@AllArgsConstructor
+public class DataInitializer implements ApplicationRunner {
+
+    private UserService userService;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        var userData = new UserCreateDTO();
+        userData.setEmail("hexlet@example.com");
+        userData.setPassword("qwerty");
+        userData.setFirstName("Ryan");
+        userData.setLastName("Gosling");
+        userService.createUser(userData);
+    }
+}
--- app/src/main/java/hexlet/code/model/User.java
+++ app/src/main/java/hexlet/code/model/User.java
@@ -9,6 +9,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -26,6 +27,7 @@ import java.time.LocalDate;
 @Getter
 @Setter
 @NoArgsConstructor
+@EqualsAndHashCode
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
